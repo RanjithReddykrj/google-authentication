1739e3b96da1403dd2b9b37d8c3ad161ddf8c3b9
Ranjithreddy <ranjithreddy.rjk@gmail.com>
2023-06-14 23:28:09 +0530

    google backend commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useEffect} from 'react'
+import jwt_decode from "jwt-decode"
+import { useState } from 'react';
 function App() {
+  const [user,setUser]=useState({});
+  function handleCallbackResponse(response){
+    const userdata=jwt_decode(response.credential);
+    setUser(userdata);
+    document.getElementById('signInDiv').hidden=true;
+  }
+  function signOut(){
+    setUser({});
+  }
+  useEffect(() => {
+    /*global google */
+    google.accounts.id.initialize({
+      client_id: "94063395624-334lr88jg7o44t2h9n8t5rmacfoml3gt.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+  
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+    google.accounts.id.prompt();
+  }, );
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        Object.keys(user).length === 0 &&
+        <div id="signInDiv"></div>
+      }
+             
+      
+      {
+        Object.keys(user).length !==0 && <div>
+        <img src={user.picture}></img>
+        <h3>{user.name}</h3>
+        <button onClick={signOut}>Signout</button>
+        
+      </div> 
+      
+      
+        }
+
     </div>
   );
 }
